6bdc832efd039e9531bcf7da0e14ed142ecd6978
Amra <amra.dedic@edu.fit.ba>
2022-10-24 14:43:05 +0200

    search bar

--- app/src/main/java/com/example/pokedex/PokedexApplication.kt
+++ app/src/main/java/com/example/pokedex/PokedexApplication.kt
@@ -1,9 +1,11 @@
 package com.example.pokedex
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-class PokedexApplication:Application() {
+@HiltAndroidApp
+class PokedexApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
