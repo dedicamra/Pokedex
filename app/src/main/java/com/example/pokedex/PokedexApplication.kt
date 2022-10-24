b7fb973f7176f2226744fc5b304087104093f18e
Amra <amra.dedic@edu.fit.ba>
2022-10-24 13:44:08 +0200

    retrofit setup

---
+++ app/src/main/java/com/example/pokedex/PokedexApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pokedex
+
+import android.app.Application
+import timber.log.Timber
+
+class PokedexApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
